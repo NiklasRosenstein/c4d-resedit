NiklasRosenstein/c4d-resedit

commit bafe93366885beb63d98cf0d0a5ddbe44c9094a0

Add "Dual Icons" option to Toolbar window
* Constructor for ResEditPrefs
* ResEditPrefs::toolbarIconSingle

source/ResEdit.cpp
@@ -45,8 +45,9 @@ void SaveResEditPrefs()
 			pf->WriteFilename(g_pResEditPrefs->fnNewZip);
 			pf->WriteFilename(g_pResEditPrefs->fnDestZip);
 			g_pResEditPrefs->arOldFiles.Write(pf, 0);
-			pf->WriteInt32(g_pResEditPrefs->toolbarIconSize);
-			pf->WriteInt32(g_pResEditPrefs->toolbarIconOrder);
+			pf->WriteBool(g_pResEditPrefs->toolbarIconSize);
+			pf->WriteBool(g_pResEditPrefs->toolbarIconOrder);
+			pf->WriteBool(g_pResEditPrefs->toolbarIconSingle);
 			pf->Close();
 		}
 	}
@@ -99,8 +100,9 @@ Bool PluginStart()
 		pf->ReadFilename(&g_pResEditPrefs->fnNewZip);
 		pf->ReadFilename(&g_pResEditPrefs->fnDestZip);
 		g_pResEditPrefs->arOldFiles.Read(pf);
-		pf->ReadInt32(&g_pResEditPrefs->toolbarIconSize);
-		pf->ReadInt32(&g_pResEditPrefs->toolbarIconOrder);
+		pf->ReadBool(&g_pResEditPrefs->toolbarIconSize);
+		pf->ReadBool(&g_pResEditPrefs->toolbarIconOrder);
+		pf->ReadBool(&g_pResEditPrefs->toolbarIconSingle);
 		pf->Close();
 	}
 	ReadDistriPrefs();

source/ResEditToolBar.cpp
@@ -46,6 +46,7 @@ enum {
 	IDC_ICONSIZE_LARGE,
 	IDC_ICONORDER_HORIZ,
 	IDC_ICONORDER_VERT,
+	IDC_ICONSINGLE,
 	IDC_ICONGROUP,
 
 	IDC_DUMMY // just for the mac comiler :-)
@@ -156,6 +157,10 @@ Bool CResEditToolBar::Command(Int32 lID, const BaseContainer &msg)
 			g_pResEditPrefs->toolbarIconOrder = lID;
 			UpdateToolbarIcons();
 			break;
+		case IDC_ICONSINGLE:
+			g_pResEditPrefs->toolbarIconSingle = !g_pResEditPrefs->toolbarIconSingle;
+			UpdateToolbarIcons();
+			break;
 	}
 	return true;
 }
@@ -222,9 +227,10 @@ void CResEditToolBar::UpdateToolbarIcons()
 			break;
 	}
 
-	Int32 cols = 2, rows = 0;
+	Int32 const n = (g_pResEditPrefs->toolbarIconSingle ? 1 : 2);
+	Int32 cols = n, rows = 0;
 	if (iconOrderMode != IDC_ICONORDER_VERT) {
-		cols = 0, rows = 2;
+		cols = 0, rows = n;
 		iconOrderMode = IDC_ICONORDER_HORIZ;
 	}
 
@@ -247,13 +253,15 @@ void CResEditToolBar::UpdateToolbarIcons()
 
 	// Update the menus.
 	MenuFlushAll();
-	MenuSubBegin("Edit");
+	MenuSubBegin("View");
 	MenuAddString(IDC_ICONSIZE_SMALL, check("Small Icons", iconSizeMode == IDC_ICONSIZE_SMALL));
 	MenuAddString(IDC_ICONSIZE_MEDIUM, check("Medium Icons", iconSizeMode == IDC_ICONSIZE_MEDIUM));
 	MenuAddString(IDC_ICONSIZE_LARGE, check("Large Icons", iconSizeMode == IDC_ICONSIZE_LARGE));
 	MenuAddSeparator();
 	MenuAddString(IDC_ICONORDER_HORIZ, check("Horizontal Layout", iconOrderMode == IDC_ICONORDER_HORIZ));
 	MenuAddString(IDC_ICONORDER_VERT, check("Vertical Layout", iconOrderMode == IDC_ICONORDER_VERT));
+	MenuAddSeparator();
+	MenuAddString(IDC_ICONSINGLE, check("Dual Icons", !g_pResEditPrefs->toolbarIconSingle));
 	MenuSubEnd();
 	MenuFinished();
 }

source/globals.h
@@ -81,7 +81,14 @@ struct ResEditPrefs
 	String strOrigPath, strNewPath, strExtractPath, strWizardPath;
 	Filename fnNewZip, fnDestZip;
 	CDynamicFilenameSet arOldFiles;
-	Int32 toolbarIconSize, toolbarIconOrder;
+	Int32 toolbarIconSize, toolbarIconOrder, toolbarIconSingle;
+
+	ResEditPrefs()
+		: strOrigPath(), strNewPath(), strExtractPath(), strWizardPath(),
+			fnNewZip(), fnDestZip(),
+			arOldFiles(),
+			toolbarIconSize(NOTOK), toolbarIconOrder(NOTOK), toolbarIconSingle(false)
+	{ }
 };
 extern ResEditPrefs *g_pResEditPrefs;
 

commit 1e5338379a7151298de7e0e75cfa7965644f0462

Fix bug introduced in bafe933, icon order and size must be saved as ints

source/ResEdit.cpp
@@ -45,8 +45,8 @@ void SaveResEditPrefs()
 			pf->WriteFilename(g_pResEditPrefs->fnNewZip);
 			pf->WriteFilename(g_pResEditPrefs->fnDestZip);
 			g_pResEditPrefs->arOldFiles.Write(pf, 0);
-			pf->WriteBool(g_pResEditPrefs->toolbarIconSize);
-			pf->WriteBool(g_pResEditPrefs->toolbarIconOrder);
+			pf->WriteInt32(g_pResEditPrefs->toolbarIconSize);
+			pf->WriteInt32(g_pResEditPrefs->toolbarIconOrder);
 			pf->WriteBool(g_pResEditPrefs->toolbarIconSingle);
 			pf->Close();
 		}
@@ -100,8 +100,8 @@ Bool PluginStart()
 		pf->ReadFilename(&g_pResEditPrefs->fnNewZip);
 		pf->ReadFilename(&g_pResEditPrefs->fnDestZip);
 		g_pResEditPrefs->arOldFiles.Read(pf);
-		pf->ReadBool(&g_pResEditPrefs->toolbarIconSize);
-		pf->ReadBool(&g_pResEditPrefs->toolbarIconOrder);
+		pf->ReadInt32(&g_pResEditPrefs->toolbarIconSize);
+		pf->ReadInt32(&g_pResEditPrefs->toolbarIconOrder);
 		pf->ReadBool(&g_pResEditPrefs->toolbarIconSingle);
 		pf->Close();
 	}